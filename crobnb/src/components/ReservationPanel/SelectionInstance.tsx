acd391af344ebfa5326a5e66a8cccd4603bff8a8
UltraXDZN <ultraxdzn@gmail.com>
2025-03-07 16:40:18 +0100

    Added Dropdown with different states which are triggered by bools: - added the fourth option for incrementing and decrementing a number of guests - created a component variations and simplified all of them so that each require it's own props - fixed a bug where it was allowed to open multiple dropdowns at the same time

--- crobnb/src/components/ReservationPanel/SelectionInstance.tsx
+++ crobnb/src/components/ReservationPanel/SelectionInstance.tsx
@@ -6,32 +6,52 @@ import bed from '@assets/icons/bed.svg'
 import date from '@assets/icons/date.svg'
 import person from '@assets/icons/person.svg'
 
+type DropdownId = 'location' | 'icon' | 'date' | 'guests';
 
 
 const SelectionInstance: React.FC = () => {
+    const [activeDropdown, setActiveDropdown] = React.useState<DropdownId | null>(null);
+    const handleToggleDropdown = (dropdownId: DropdownId) => {
+        if (activeDropdown === dropdownId) {
+            setActiveDropdown(null);
+        } else {
+            setActiveDropdown(dropdownId);
+        }
+    };
     return (
         <div className='flex flex-row gap-4'>
             <ReservationDropdown
+                variant="icon"
                 title="Lokacija"
                 icon={location}
                 options={['Središnja Dalmacija', 'Split i okolica', 'Omiš i okolica', 'Makarska i okolica']}
-                hasStartingIcon={true}
+                isOpen={activeDropdown === 'location'}
+                onToggle={() => handleToggleDropdown('location')}
             />
             <ReservationDropdown
+                variant="standard"
                 title="Tip smještaja"
                 icon={bed}
                 options={['Hoteli', 'Apartmani', 'Turistička naselja', 'Ville', 'Mobile kućice']}
+                isOpen={activeDropdown === 'icon'}
+                onToggle={() => handleToggleDropdown('icon')}
+                
             />
             <ReservationDropdown
                 title="Datum prijave / odjave"
                 icon={date}
-                isCalendar={true}
-                options={['Option 1', 'Option 2', 'Option 3']}
+                variant="calendar"
+                isOpen={activeDropdown === 'date'}
+                onToggle={() => handleToggleDropdown('date')}
             />
             <ReservationDropdown
-                title="Broj gostiju"
+                variant='guests'
+                title="Gosti"
                 icon={person}
-                options={['Option 1', 'Option 2', 'Option 3']}
+                minValue={0}
+                maxValue={10}
+                isOpen={activeDropdown === 'guests'}
+                onToggle={() => handleToggleDropdown('guests')}
             />
         </div>
     );
